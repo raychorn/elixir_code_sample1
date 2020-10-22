dd98716dc540d941d3b4fe0a13b5818aff9d9a03
Ray C Horn <raychorn@gmail.com>
2020-10-22 11:53:32 +0000

    added more code.

--- main.exs
+++ main.exs
@@ -72,3 +72,18 @@ IO.inspect a = [0, 1, 2, 2, 2, 2, 1, 9, 3.5, 5, 8, 4, 7, 0, 6]
 {b, writes} = Sort.cycleSort(a)
 IO.puts "writes : #{writes}"
 IO.inspect b
+
+
+defmodule RC do
+  def is_perfect(1), do: false
+  def is_perfect(n) when n > 1 do
+    Enum.sum(factor(n, 2, [1])) == n
+  end
+ 
+  defp factor(n, i, factors) when n <  i*i   , do: factors
+  defp factor(n, i, factors) when n == i*i   , do: [i | factors]
+  defp factor(n, i, factors) when rem(n,i)==0, do: factor(n, i+1, [i, div(n,i) | factors])
+  defp factor(n, i, factors)                 , do: factor(n, i+1, factors)
+end
+ 
+IO.inspect (for i <- 1..10000, RC.is_perfect(i), do: i)
